c6d39a1c9ee909ff2c317889e1f20a6e1f50b11a
Andremaxico <bestthebeschan@gmail.com>
2022-07-15 14:33:26 +0200

    53: class components, users list to class component

--- src/components/Users/UsersList/UsersList.jsx
+++ src/components/Users/UsersList/UsersList.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import User from '../User';
 import classes from '../Users.module.scss';
 import * as axios from 'axios';
 
-const UsersList = (props) => {
-	if(props.usersData.length < 1) {
-		axios.get('https://social-network.samuraijs.com/api/1.0/users')
-			.then(res => {
-				props.setUsers(res.data.items)
-			})
+class UsersList extends React.Component {
+	componentDidMount() {
+		if(this.props.usersData.length < 1) {
+			axios.get('https://social-network.samuraijs.com/api/1.0/users')
+				.then(res => {
+					this.props.setUsers(res.data.items)
+				})
 		}
-	const list = props.usersData.map(data => <User info={data} key={data.id} follow={props.follow} unfollow={props.unfollow}/>);
-	return (
-		<div className={classes.UsersList}>
-			{ list }
-		</div>
-	)
+	}
+	render() {
+		const list = this.props.usersData.map(data => <User info={data} key={data.id} follow={this.props.follow} unfollow={this.props.unfollow}/>);
+
+		return (
+			<div className={classes.UsersList}>
+				{ list }
+			</div>
+		)
+	}
 }
 
 export default UsersList;
