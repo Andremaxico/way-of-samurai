cc4136655533249229f7a0e690aa09a5de5221a1
Andremaxico <bestthebeschan@gmail.com>
2022-08-05 12:26:49 +0200

    94: added react.lazy loading

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import './App.scss';
 //hocs
 import withNetworkRedirect from './hocs/withNetworkRedirect';
 import withRouter from './hocs/withRouter';
+import withSuspense from './hocs/withSuspense';
 //componenst
 import HeaderContainer from './components/Header';
-import Sidebar from './components/Sidebar/Sidebar'
-import Messages from './components/Messages/Messages';
+import Sidebar from './components/Sidebar/Sidebar';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import Users from './components/Users';
-import ProfileContainer from './components/Profile';
-import Login from './components/Login';
 import Preloader from './UI/Preloader';
 //libraries
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
@@ -20,10 +18,21 @@ import { Provider } from 'react-redux';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { initApp } from './Redux/app-reducer';
+
+//lazy components
+const ProfileContainer = React.lazy(() => import('./components/Profile'));
+const Login = React.lazy(() => import('./components/Login'));
+const Messages = React.lazy(() => import('./components/Messages'));
+
+
 const App = (props) => {
   useEffect(() => {
     props.initApp();
   }, []);
+
+  const ProfileSuspensed = withSuspense(ProfileContainer);
+  const MessagesSuspensed = withSuspense(Messages);
+  const LoginSuspensed = withSuspense(Login);
 
   if(!props.isInitSuccess) return <Preloader />;
 
@@ -33,15 +42,15 @@ const App = (props) => {
         <Sidebar data={props.sidebar}/>
         <div className='content'>
             <Routes>
-              <Route path='/profile' element={<ProfileContainer />}>
+              <Route path='/profile' element={<ProfileSuspensed />}>
                 <Route path=':userId'/>
               </Route>
-              <Route path='/messages/*' element={<Messages />} />
+              <Route path='/messages/*' element={<MessagesSuspensed />} />
               <Route path='/users/*' element={<Users />}/>
               <Route path='/news/*' element={<News />} />
               <Route path='/music/*' element={<Music />} />
               <Route path='/settings/*' element={<Settings />} />
-              <Route path='/login/*' element={<Login /> } />
+              <Route path='/login/*' element={<LoginSuspensed /> } />
             </Routes>
         </div>
       </div>
